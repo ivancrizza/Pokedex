1d5dd6d32b3aa6a6221ce338d96bdc5b6fa6b898
pizzacrizza <ivan.alineri@outlook.com>
2022-07-17 16:51:52 -0300

    entr

--- app/src/main/java/com/crizza/pokedex/PokedexApp.kt
+++
@@ -1,7 +0,0 @@
-package com.crizza.pokedex
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PokedexApp: Application()
--- app/src/main/java/com/crizza/pokedex/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.crizza.pokedex.utils
-
-object Constants {
-    const val BASE_URL = "https://pokeapi.co/api/v2/"
-}
